72226089e065b7f38a73dd1f60037fac1c290af3
Zach Sarkis <zach.sarkis@cognex.com>
2020-01-13 11:31:01 -0700

    Initial Commit

---
+++ polls/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+import datetime
+from django.urls import reverse
+from django.utils import timezone
+from .models import Question
+
+# Create your tests here.
+def create_question(question_text, days):
+   time = timezone.now() + datetime.timedelta(days=days)
+   return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+   def test_index_view_with_no_questions(self):
+       """
+       If no questions exist, an appropriate message should be displayed.
+       """
+       response = self.client.get(reverse('index'))
+       self.assertEqual(response.status_code, 200)
+       self.assertContains(response, "No polls are available.")
+       self.assertQuerysetEqual(response.context['latest_question_list'], [])
